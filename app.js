14c0b6bc1d051b7edc72ab694f5bd1895a7f2d57
deesii <donnagph@hotmail.com>
2024-07-05 18:43:32 +0100

    basic functioning chat app with tracked behaviour

--- app.js
+++ app.js
@@ -1,19 +1,34 @@
 const express = require('express');
 const http = require('http');
 const socketIO = require('socket.io');
-
-const app = express();
-const server = http.createServer(app);
+const app = express(); 
+const server = http.createServer(app); 
 app.use(express.static('public'));
 const io = socketIO(server);
 
-// TODO add event listener for socket.io connections
+// Event listener for socket.io connections
+
+io.on('connection', socket => {
+    console.log('There is now a connection');
+
+    // listens out for newMessage event, it will send the data to the client
+    socket.on('newMessage', data => {
+        io.emit('dispenseMessage', data);
+        const { sender , message } = data;
+        console.log(`${sender}: ${message}`);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('a user has disconnected')
+    });
+});
 
 app.get('/', (req, res) => {
     // Send the HTML file as the response
     res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
+// start server and listening on port 3000 
 server.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
--- public/main.js
+++ public/main.js
@@ -1,24 +1,51 @@
-// init socket.io
+// connect client to socket.io server
 const socket = io();
  
 // Get the input and button elements
-const messageInput = document.querySelector('#message-input');
+const messageInput = document.querySelector('#message');
 const button = document.querySelector('button');
+const nameInput = document.querySelector('#name');
 
 // Function to handle sending a message
 function sendMessage() {
     // Get the message text from the input
-    const message = messageInput.value;
+    message = messageInput.value;
+    nameSender = nameInput.value;
 
-    // TODO send message to server
+    // Send message to server
+    socket.emit('newMessage', {
+        sender: nameSender,
+        message: message
+    });
+   
+    console.log(`The message I am sending to server is: "${message}"`);
 
     // Clear the input field
     messageInput.value = '';
 }
 
-// TODO create function to add a new message to the chat
+// Function to add a new message to the chat
 
-// Add event listener to the button for sending a message
-button.addEventListener('click', sendMessage);
+function addMessage(){
+    socket.on('dispenseMessage', data => {
+        const { sender , message } = data;
+        console.log(`I have received the information: "${message}"`);
+    
+        // make a message element
+        const messageElement = document.createElement('div');
+        messageElement.classList.add('message');
+        messageElement.textContent = `${sender} : ${message}`;
+    
+        // add to container
+        const messageContainer = document.querySelector('.message-container');
+        messageContainer.appendChild(messageElement);
+    })
+}
 
-// TODO add event listener for new messages
+// Add event listeners to the button for sending message and adding message to chat
+function handleClick(event) {
+    sendMessage(event);
+    addMessage(event);
+}
+
+button.addEventListener('click', handleClick);
